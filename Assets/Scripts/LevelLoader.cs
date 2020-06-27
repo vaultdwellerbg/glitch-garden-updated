e875d2ea27df49ad10005ec17afe4a22d2ad280d
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-06-27 17:33:21 +0300

    Init project

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float spashScreenDuration = 3f;
+
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine("LoadStartScreen");
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private IEnumerator LoadStartScreen()
+    {
+        yield return new WaitForSeconds(spashScreenDuration);
+
+        LoadNextScene();
+    }
+}
