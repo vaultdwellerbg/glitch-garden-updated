5ca0757ae69b69672448e514bf9426f8ee7954b9
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-07-11 20:57:27 +0300

    Add movement to lizard attacker

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 3f)] [SerializeField] float walkSpeed = 1f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * walkSpeed);
+    }
+}
