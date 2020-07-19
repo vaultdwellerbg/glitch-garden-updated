db7ef772b0230e721138c8d92833815ab81a196a
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-07-19 14:20:22 +0300

    Instantiate projectiles from defender

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+	[SerializeField] GameObject projectilePrefab;
+
+	public void Fire()
+	{
+		var gunObject = transform.Find("Gun");
+		Instantiate(projectilePrefab, gunObject.transform.position, Quaternion.identity);
+	}
+}
