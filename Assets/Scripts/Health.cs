3635477556fa7be2166b33c5b8a54adfed14f9c6
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-07-23 00:23:36 +0300

    Handle destroying of attackers

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,13 +3,23 @@
 public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFXPrefab;
 
     public void DealDamage(float value)
     {
         health -= value;
         if (health <= 0)
         {
+            TriggerDeathVFX();
             Destroy(gameObject);
         }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFXPrefab) return;
+
+        GameObject deathVFX = Instantiate(deathVFXPrefab, transform.position, Quaternion.identity);
+        Destroy(deathVFX, 1f);
     }
 }
