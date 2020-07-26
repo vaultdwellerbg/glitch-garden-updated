109bdea96fe0279250aa21ea3dacd91dfa6c1fd3
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-07-26 19:25:50 +0300

    Add UI for selecting defenders

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+	private void OnMouseDown()
+	{
+		var defenderButtons = FindObjectsOfType<DefenderButton>();
+		foreach (DefenderButton button in defenderButtons)
+		{
+			button.GetComponent<SpriteRenderer>().color = new Color32(51, 51, 51, 255);
+		}
+		GetComponent<SpriteRenderer>().color = Color.white;
+	}
+}
