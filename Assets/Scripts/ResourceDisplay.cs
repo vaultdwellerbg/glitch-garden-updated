bc71afb5ccffc9c5b76dda96cb90e81fb1f143ab
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-07-30 00:12:24 +0300

    Create resource system foundation

---
+++ Assets/Scripts/ResourceDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceDisplay : MonoBehaviour
+{
+	[SerializeField] int gold = 100;
+
+	Text starText;
+
+	private void Start()
+	{
+		starText = GetComponent<Text>();
+		UpdateDispay();
+	}
+
+	public void AddGold(int value)
+	{
+		gold += value;
+		UpdateDispay();
+	}
+
+	public void SpendGold(int value)
+	{
+		if (gold >= value)
+		{
+			gold -= value;
+			UpdateDispay();
+		}
+	}
+
+	private void UpdateDispay()
+	{
+		starText.text = gold.ToString();
+	}
+}
