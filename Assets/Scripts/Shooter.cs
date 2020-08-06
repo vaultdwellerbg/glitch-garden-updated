49f60ea59baa2ade473da71bd738ee1693bdb554
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-08-07 00:17:55 +0300

    Detect attackers on same line with shooter defender

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
@@ -7,6 +8,25 @@ public class Shooter : MonoBehaviour
 	[SerializeField] float speed = 1f;
 	[SerializeField] float damage = 50f;
 
+	private AttackerSpawner laneAttackerSpawner;
+
+	private void Start()
+	{
+		SetLaneAttackerSpawner();
+	}
+
+	private void Update()
+	{
+		if (AttackerInLane())
+		{
+			Debug.Log("Shoot");
+		}
+		else
+		{
+			Debug.Log("Wait");
+		}
+	}
+
 	public void Fire()
 	{
 		var gunObject = transform.Find("Body").Find("Gun");
@@ -14,5 +34,29 @@ public class Shooter : MonoBehaviour
 		var projectileScript = projectile.GetComponent<Projectile>();
 		projectileScript.SetMovementSpeed(speed);
 		projectileScript.SetDamage(damage);
+	}
+
+	private void SetLaneAttackerSpawner()
+	{
+		var attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+		foreach (var attackerSpawner in attackerSpawners)
+		{
+			StoreAttackerSpawnerIfOnCurrentLane(attackerSpawner);
+		}
+	}
+
+	private void StoreAttackerSpawnerIfOnCurrentLane(AttackerSpawner attackerSpawner)
+	{
+		bool isSpawnerCloseEnough = (Mathf.Abs(attackerSpawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+		if (isSpawnerCloseEnough)
+		{
+			laneAttackerSpawner = attackerSpawner;
+		}
+	}
+
+	private bool AttackerInLane()
+	{
+		var attackersCount = laneAttackerSpawner.transform.childCount;
+		return attackersCount > 0;
 	}
 }
