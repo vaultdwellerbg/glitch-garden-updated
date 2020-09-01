95c326a22d43042450a7aa0167f4232aac5abde2
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-09-02 00:30:57 +0300

    Recreate and complete fox attacker type

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+	Attacker attacker;
+
+	private void Start()
+	{
+		attacker = GetComponent<Attacker>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		GameObject collidedWith = collider.gameObject;
+
+		if (!collidedWith.GetComponent<Defender>()) return;
+
+		if (collidedWith.name.StartsWith("Gravestone"))
+		{
+			GetComponent<Animator>().SetTrigger("jumpTrigger");
+		}
+		else if (attacker)
+		{
+			attacker.Attack(collidedWith);
+		}
+		else
+		{
+			Debug.LogError("Missing Attacker script!");
+		}
+	}
+}
