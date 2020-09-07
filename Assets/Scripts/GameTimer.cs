524dc440e6d8ed178d3c509893650fb22f9a69d0
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-09-07 19:24:15 +0300

    Add level timer UI

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("The level timer in seconds.")]
+    [SerializeField] float levelTime = 10f;
+
+	private void Update()
+	{
+		bool timerFinished = Time.timeSinceLevelLoad >= levelTime;
+		if (timerFinished)
+		{
+			Debug.Log("Level timer finished.");
+			return;
+		}
+
+		GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+	}
+}
