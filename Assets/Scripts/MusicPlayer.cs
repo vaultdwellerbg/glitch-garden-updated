468b3b253f4d8fa497106f6fe7ae67082b4ec162
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-09-19 18:27:06 +0300

    Add control for master volume from options screen

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	AudioSource audioSource;
+
+	private void Start()
+	{
+		DontDestroyOnLoad(this);
+		audioSource = GetComponent<AudioSource>();
+		audioSource.volume = PlayerPrefsController.GetMasterVolume();
+	}
+
+	public void SetVolume(float volume)
+	{
+		audioSource.volume = volume;
+	}
+}
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float defaultVolume = 0.8f;
+
+    private MusicPlayer musicPlayer;
+
+    private void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+        musicPlayer = FindObjectOfType<MusicPlayer>();
+    }
+
+    private void Update()
+    {
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogError("Music player is missing");
+        }
+    }
+
+    public void SaveOptions()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+    }
+
+    public void SetDefaultOptions()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
