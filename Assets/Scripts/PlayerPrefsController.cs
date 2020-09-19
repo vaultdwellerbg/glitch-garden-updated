f6b6292ac26772b786c4e88cd2fa1627ea9cffad
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-09-19 17:51:07 +0300

    Add PlayerPrefs wrapper

---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+    const string MASTER_VOLUME_KEY = "masterVolume";
+    const string DIFFICULTY_KEY = "difficulty";
+
+    const float MIN_VOLUME = 0f;
+    const float MAX_VOLUME = 1f;
+
+    public static void SetMasterVolume(float volume)
+    {
+        if (volume >= MIN_VOLUME && volume <= MAX_VOLUME)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Master volume is out of range");
+        }
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+}
